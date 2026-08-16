9500259c3ce217b998bbe6ec510a1fa7a0fa0830
seroxdesign <sherifcherfa@gmail.com>
2026-08-16 11:04:03 -0400

    Sanitize via $SSHPASS env var; remove embedded credential entirely

--- code/sync_to_github.sh
+++ code/sync_to_github.sh
@@ -63,7 +63,7 @@ for py in observe_religious.py run_jlens.py jlens_dsv4.py analyze_experts.py gen
     if [ -f "$RESEARCH_DIR/$py" ]; then
         # Sanitize: check for credentials
         if grep -qE 'in.t-4.@' "$RESEARCH_DIR/$py" 2>/dev/null; then
-            sed "s/$SSHPASS_ENV/\$SSHPASS_ENV/g" "$RESEARCH_DIR/$py" > "code/$py"
+            sed "s/$SSHPASS/\$SSHPASS_ENV/g" "$RESEARCH_DIR/$py" > "code/$py"
         else
             cp "$RESEARCH_DIR/$py" "code/$py"
         fi
@@ -73,7 +73,7 @@ done
 for sh in run_full_observation.sh run_christian_observation.sh run_jlens_tp2.sh jlens_watch.sh obs_christian_watch.sh chain_next_runs.sh finalize_christian.sh sync_to_github.sh; do
     if [ -f "$RESEARCH_DIR/$sh" ]; then
         if grep -qE 'in.t-4.@' "$RESEARCH_DIR/$sh" 2>/dev/null; then
-            sed "s/$SSHPASS_ENV/\$SSHPASS_ENV/g" "$RESEARCH_DIR/$sh" > "code/$sh"
+            sed "s/$SSHPASS/\$SSHPASS_ENV/g" "$RESEARCH_DIR/$sh" > "code/$sh"
         else
             cp "$RESEARCH_DIR/$sh" "code/$sh"
         fi
